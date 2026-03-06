98e20fb8e00846e4b5a7d8883be5ada23424ac32
Saibal1234 <saibalroy587@gmail.com>
2026-03-06 10:22:21 +0530

    push this code

---
+++ Loops/PrintSquarePattern.java
@@ -0,0 +1,11 @@
+package Loops;
+import java.util.*;
+
+public class PrintSquarePattern {
+    public static void main (String [] args){
+        int line ;
+        for(line=1 ; line<=4 ; line++){
+             System.out.println("****");
+        }
+    }
+}
